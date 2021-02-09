d7da1621eb734a2c2ccb85dc21e476aae2b21f2d
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2021-02-10 01:15:25 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cache_widget_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cache_widget_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _isLogin = false;
+
+  void login() {
+    setState(() {
+      _isLogin = true;
+    });
+  }
+
+  void logout() {
+    setState(() {
+      _isLogin = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cache Sample'),
+          actions: [
+            InkWell(
+              onTap: () {
+                _isLogin ? logout() : login();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(_isLogin ? 'ログアウト' : 'ログイン'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: CachableCounter(),
+      ),
+    );
+  }
+}
+
+class CachableCounter extends StatefulWidget {
+  @override
+  _CachableCounterState createState() => _CachableCounterState();
+}
+
+class _CachableCounterState extends State<CachableCounter> {
+  Widget _cache;
+  var _buildCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_cache != null) {
+      return _cache;
+    }
+
+    _buildCount++;
+
+    return _cache = Center(
+      child: Text(
+        'Number of build => $_buildCount',
+      ),
+    );
+  }
+}
